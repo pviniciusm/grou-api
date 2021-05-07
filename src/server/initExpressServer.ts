1dbc5fc9a3d2c82cc946a9e64060f249e0f16279
pviniciusm <pvinics@gmail.com>
2021-05-07 04:17:01 -0300

    Heroku deploy

--- src/server/initExpressServer.ts
+++ src/server/initExpressServer.ts
@@ -7,7 +7,7 @@ const initExpressServer = () => {
   app.use(cors());
   app.use(express.json());
   initRoutes(app);
-  app.listen(3333);
+  app.listen(process.env.PORT || 5000);
 
   console.log('Server is now listening...');
 };
